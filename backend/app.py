3b519312bb9326953913300816c1f7f5ba93090a
Kevin Riexinger <info@kevinriexinger.de>
2022-10-06 00:35:18 +0200

    backend

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/time")
+def get_current_time():
+    return {"time": time.asctime()}
+
+@app.route("/send")
+def send_signal():
+    return None
