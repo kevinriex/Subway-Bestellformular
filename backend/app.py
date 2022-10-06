a62a4179d6d5cfebb376a7a2a85ff6ab3719099e
Kevin Riexinger <info@kevinriexinger.de>
2022-10-06 02:34:49 +0200

    Good Night

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,40 @@
 import time
-from flask import Flask
+from urllib import request, parse
+from flask import Flask, request as fr
+import json
 
 app = Flask(__name__)
 
 @app.route("/time")
 def get_current_time():
+    return {"time": time.time()}
+
+@app.route("/asctime")
+def get_current_asctime():
     return {"time": time.asctime()}
 
-@app.route("/send")
+@app.route("/send", methods = ['POST'])
 def send_signal():
-    return None
+    content_type = fr.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        data = fr.get_json()
+        print(f"\n\ndatapre: {data}\n\n")
+        data = json.dumps(data)
+        data = data.encode("utf-8")
+        print(f"\n\ndataaft: {data}\n\n")
+        req = request.Request("https://signal.kyrtech.net/v2/send", data=data, method="POST")
+        resp = request.urlopen(req)
+        print(resp.read())
+        return 200
+    else:
+        return 'Content-Type not supported!', 500
+    
+@app.route("/pdf", methods = ["POST"])
+def pdf():
+    if (fr.headers.get('Content-Type') == 'application/json'):
+        data = fr.get_json()
+        w = open(f"data/{data['timestamp']}.txt", "w")
+        w.write(data["message"])
+        w.close()
+        print(str(data["message"]))
+        return str(data["message"])
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,52 @@
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <h1>It Works!</h1>
-      </header>
-    </div>
-  );
+    const [currentTime, setCurrentTime] = useState(0);
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch("/time")
+            .then((res) => res.json())
+            .then((data) => {
+                setCurrentTime(data.time);
+            });
+    }, []);
+    async function btnClick(e) {
+        e.preventDefault();
+        // let res = await fetch("/send", {
+        //     method: "POST",
+        //     headers:{'content-type': 'application/json'},
+        //     body: JSON.stringify({
+        //       message: message,
+        //       number: "+491746751194",
+        //       recipients: [ "+491746751194" ]
+        //     })})
+        let res = await fetch("/pdf", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({
+                message: message,
+                timestamp: currentTime,
+            })
+        })
+        alert(`${res.status} ${res.statusText}\n\ ${res.text}`)
+    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>It Works!</h1>
+                <h2>It's {currentTime}</h2>
+                <form onSubmit={btnClick}>
+                    <input
+                        type="text"
+                        onChange={(e) => setMessage(e.target.value)}
+                    ></input>
+                    <button type="submit">Submit</button>
+                </form>
+            </header>
+        </div>
+    );
 }
 
 export default App;
